5c554b038d44518a7e2ad94a5a392fd79108dcfa
algolover <rockin.divyanshu@gmail.com>
2017-06-05 21:24:31 +0530

    Revert "Latest Version of Bootstrap added"
    This reverts commit ccaa9aa0406ace82d25afa69134807e42d4c976a.

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
 <html>
 <head>
 	<title>Placeholder</title>
-	 <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
 	<div class="container-fluid	">
